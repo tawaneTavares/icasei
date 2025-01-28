8a2c218865b3f8b73fd60c12cdedecd5f9ba6577
tawaneTavares <tawane_cristina@hotmail.com>
2025-01-28 20:55:56 -0300

    add initial app bottom menu

--- app/src/main/java/com/example/icasei/MainActivity.kt
+++ app/src/main/java/com/example/icasei/MainActivity.kt
@@ -6,11 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import com.example.icasei.ui.theme.IcaseiTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +33,52 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IcaseiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val navController = rememberNavController()
+                val selected = remember {
+                    mutableStateOf(Icons.Default.Home)
+                }
+                val modifier = Modifier
+                Scaffold(modifier = modifier.fillMaxSize(),
+                    bottomBar = {
+                        BottomAppBar {
+                            BottomMenuIcon(
+                                modifier = modifier.weight(1f),
+                                icon = Icons.Default.Home,
+                                contentDescription = "Home",
+                                isSelected = (selected.value == Icons.Default.Home)
+                            ) { imageVector ->
+                                selected.value = imageVector
+                                /*todo: add navigation navController.navigate(~screen~) {
+                                //popUpTo(0)
+                                }*/
+                            }
+
+                            BottomMenuIcon(
+                                modifier = modifier.weight(1f),
+                                icon = Icons.Default.Favorite,
+                                contentDescription = "Favorite",
+                                isSelected = (selected.value == Icons.Default.Favorite)
+                            ) { imageVector ->
+                                selected.value = imageVector
+                                /*todo: add navigation navController.navigate(~screen~) {
+                                //popUpTo(0)
+                                }*/
+                            }
+
+                            BottomMenuIcon(
+                                modifier = modifier.weight(1f),
+                                icon = Icons.Default.PlayArrow,
+                                contentDescription = "My Lists",
+                                isSelected = (selected.value == Icons.Default.PlayArrow)
+                            ) { imageVector ->
+                                selected.value = imageVector
+                                /*todo: add navigation navController.navigate(~screen~) {
+                                //popUpTo(0)
+                                }*/
+                            }
+                        }
+                    }
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -28,6 +87,23 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun BottomMenuIcon(modifier: Modifier, icon: ImageVector, contentDescription: String, isSelected: Boolean, onClick: (ImageVector) -> Unit) {
+        IconButton(
+            onClick = {
+                onClick.invoke(icon)
+            },
+            modifier = modifier
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = contentDescription,
+                modifier = Modifier.size(26.dp),
+                tint = if (isSelected) Color.Black else Color.Gray,
+            )
+        }
 }
 
 @Composable
